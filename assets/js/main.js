02c7c620da1eeab3048554d5839326dc4bed78b6
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-23 23:43:41 +0100

    styleUndHtml

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
 console.log("klappts?")
+
+/* 
+Frage
+
+per Range Schriftgröße auswählen
+    rem 0-5
+
+per Dropdown Schrift-Family auswählen
+    1. BodoniModa
+    2. OpenSans
+    3. Roboto
+    4. YuseiMagic 
+    5. SansSerif ???
+
+Per Input Text eingeben
+
+Output Feld mit Formatiertem Text 
+
+
+*/
