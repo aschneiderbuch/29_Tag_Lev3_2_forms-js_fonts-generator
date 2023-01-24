2e922ec7471b3d7c56270001d5b6fa21c559d5fc
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-24 22:59:27 +0100

    function

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,38 @@ Output Feld mit Formatiertem Text
 
 */
 
+// Variablen 
+
+let inputFontSize = document.querySelector("#inputFontSize");
+let inputFontFamily = document.querySelector("#inputFontFamily");
+let inputText = document.querySelector("#inputText");
+
+console.log(inputFontSize, inputFontFamily, inputText);
+
+let outputFeld = document.querySelector("#outputFeld");
+
+// Eventlistener
+let aktivate = inputFontFamily.addEventListener("change", neuesFormatierung);
+    aktivate = inputFontSize.addEventListener("change", neuesFormatierung);
+    aktivate = inputText.addEventListener("change", neuesFormatierung);
+
+
+// Funktion neueFromatierung
+
+function neuesFormatierung() {
+console.log("in Funktion + eventlistener geht");
+
+inputGröße = inputFontSize.value;
+console.log(inputGröße);
+inputSchriftArt = inputFontFamily.value;
+console.log(inputSchriftArt);
+inputBuchstaben = inputText.value;
+console.log(inputBuchstaben);
+
+ergebnis = `font-size:${inputGröße}rem; ${inputSchriftArt} `;
+console.log(ergebnis);
+
+outputFeld.innerHTML = inputBuchstaben;
+outputFeld.style = ergebnis;
+
+}
